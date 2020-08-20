8b5bb5387a9bb78e46d4109a41cf5b4964f1e302
2016118@iiitdmj.ac.in <39498672+katahrekartik@users.noreply.github.com>
2020-08-20 17:39:14 +0530

    Updates

---
+++ precache-manifest.1de107d872bb5ac37b885c7416123a10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8de8279af5548f4b07f0c4c305051f6f",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6e85785576653a492b78",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fb406ab04d356630773c",
+    "url": "/portfolio/static/css/main.6931aeec.chunk.css"
+  },
+  {
+    "revision": "6e85785576653a492b78",
+    "url": "/portfolio/static/js/2.29bf4d6a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.29bf4d6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb406ab04d356630773c",
+    "url": "/portfolio/static/js/main.9c3f8001.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
